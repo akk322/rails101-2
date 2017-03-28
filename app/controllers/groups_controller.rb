8fd9fb7bcd9820a1d879dd2ad7f5c093785b54ad
jensen liang <liangjianjiannick@126.com>
2017-03-29 07:30:59 +0800

    implement my group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,6 +27,7 @@ class GroupsController < ApplicationController
    else
      render :new
    end
+
  end
 
 def update
@@ -51,21 +52,26 @@ def destroy
 end
 
 def join
-  @group =Group.find(params[:id])
-  if !current_user.is_member_of?(@group)
-    current_user.join!(@group)
-    flash[:notice]="加入本讨论版成功！"
-  else
-    flash[:warning]="你已经是本讨论版成员了！"
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
+
+    redirect_to group_path(@group)
   end
 
   def quit
-    @group =Group.find(params[:id])
+    @group = Group.find(params[:id])
+
     if current_user.is_member_of?(@group)
       current_user.quit!(@group)
-      flash[:alert]="已退出本讨论版！"
+      flash[:alert] = "已退出本讨论版！"
     else
-      flash[:waring]="你不是本讨论版成员，怎么退出 XD"
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
     end
 
     redirect_to group_path(@group)
@@ -84,5 +90,5 @@ def join
  def group_params
    params.require(:group).permit(:title, :description)
  end
-
-end
+ 
+ end
